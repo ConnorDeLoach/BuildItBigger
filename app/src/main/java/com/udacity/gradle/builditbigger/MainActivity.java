9d772aa59873ea363275358a42e9d4dc92d853f2
Connor DeLoach <connordeloach@gmail.com>
2017-03-15 14:08:51 -0400

    Adds google cloud messaging module backend - backend: adds method sayJoke() to retrieve joke from MyJokes library - app: adds AsyncTask to execute sayJoke() from backend and start JokeActivity from displayjoke android library.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,16 +1,12 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.MyJokes.MyJokes;
 import com.google.android.gms.ads.MobileAds;
-
-import comconnordeloach.httpsgithub.displayjoke.JokeActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -47,11 +43,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        String joke = MyJokes.getJoke();
-        Intent intent = new Intent(this, JokeActivity.class);
-        intent.putExtra("joke", joke);
-
-        startActivity(intent);
+        new EndpointsAsyncTask().execute(this);
     }
 
 
